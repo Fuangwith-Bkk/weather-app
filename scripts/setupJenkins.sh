6e4d255db8eb458f88db16f3cc361a27235e1129
Fuangwit Sopharat <fuangwith@gmail.com>
2020-07-12 20:19:03 +0700

    build pipeline

--- scripts/setupJenkins.sh
+++ scripts/setupJenkins.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-oc new-project demo-jenkins --display-name "Jenkins"
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
-oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m
+export NS_JENKINS="demo-jenkins"
+
+oc new-project ${NS_JENKINS} --display-name "Jenkins"
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${NS_JENKINS}
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${NS_JENKINS}
 
 
 oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-maven:4.1.0\n
@@ -12,10 +14,12 @@ oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-
    curl http://mirror.centos.org/centos-7/7/os/x86_64/RPM-GPG-KEY-CentOS-7 -o /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7 && \ \n
    DISABLES="--disablerepo=rhel-server-extras --disablerepo=rhel-server --disablerepo=rhel-fast-datapath --disablerepo=rhel-server-optional --disablerepo=rhel-server-ose --disablerepo=rhel-server-rhscl" && \ \n
    yum $DISABLES -y --setopt=tsflags=nodocs install skopeo && yum clean all\n
-   USER 1001' --name=jenkins-agent-appdev -n demo-jenkins
+   USER 1001' --name=jenkins-agent-appdev -n ${NS_JENKINS}
    
 oc get is
 oc get pods -w
 
-oc apply -f manifests/jenkins-configmap.yaml -n demo-jenkins
+oc apply -f manifests/jenkins-configmap.yaml -n ${NS_JENKINS}
+oc apply -f manifests/weather-bc-pipeline.yaml -n ${NS_JENKINS}
+
 echo "Jenkins Url = $(oc get route jenkins -n demo-jenkins -o jsonpath='{.spec.host}') "
