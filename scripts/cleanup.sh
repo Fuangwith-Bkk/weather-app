c9f42f8ffc143360a8fa6ecf4443815d1d68bb7a
Fuangwith-Bkk <fuangwith@gmail.com>
2022-09-18 00:08:52 +0700

    pwd

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-oc delete project demo-nexus
-oc delete project demo-sonarqube
-oc delete project demo-jenkins
+oc delete project demo-weather-ci-cd
 oc delete project demo-weather-dev
 oc delete project demo-weather-prod
 
